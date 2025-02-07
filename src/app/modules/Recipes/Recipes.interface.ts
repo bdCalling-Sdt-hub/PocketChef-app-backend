5131e4f88e6461279434532a64f1ef9e6fa85c05
Md Abdur Razzak <rakibt23p@gmail.com>
2025-02-07 12:20:25 +0600

    create interface

---
+++ src/app/modules/Recipes/Recipes.interface.ts
@@ -0,0 +1,25 @@
+export type IRecipes = {
+    recipeName: string;
+    description: string;
+    instructions: [string]
+
+
+}
+
+
+// name: String,
+// description: String,
+// portionSize: Number,
+// level: String,
+// course: String,
+// keyIngredient: String,
+// dietType: String,
+// cookingMethod: String,
+// totalTime: String,
+// prepTime: String,
+// cookTime: String,
+// instructions: [String],
+// ingredients: [{ name: String, amount: Number, unit: String }],
+// tags: [String],
+// images: [String],
+// createdAt: { type: Date, default: Date.now }
