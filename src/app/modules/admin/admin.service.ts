25e37f407c6561749b3d70c2d97216ffdff81c53
Md Abdur Razzak <rakibt23p@gmail.com>
2025-02-18 16:42:19 +0600

    work on get filter awais new singup user

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
-import ApiError from '../../../errors/ApiError';
 import { IUser } from '../user/user.interface';
 import { User } from '../user/user.model';
+import ApiError from '../../../errors/ApiErrors';
 
 const createAdminToDB = async (payload: IUser): Promise<IUser> => {
     const createAdmin: any = await User.create(payload);
@@ -10,9 +10,9 @@ const createAdminToDB = async (payload: IUser): Promise<IUser> => {
     }
     if (createAdmin) {
         await User.findByIdAndUpdate(
-        { _id: createAdmin?._id },
-        { verified: true },
-        { new: true }
+            { _id: createAdmin?._id },
+            { verified: true },
+            { new: true }
         );
     }
     return createAdmin;
@@ -30,6 +30,34 @@ const getAdminFromDB = async (): Promise<IUser[]> => {
     const admins = await User.find({ role: 'ADMIN' })
         .select('name email profile contact location');
     return admins;
+};
+
+// get all user from database
+interface IFilter {
+    filter: 'weekly' | 'monthly' | 'yearly';
+}
+
+const getAllNewUser = async (filter: IFilter['filter']): Promise<IUser[]> => {
+    let startDate: Date;
+    const now = new Date();
+    if (filter === 'weekly') {
+        startDate = new Date();
+        startDate.setDate(now.getDate() - 7);
+    } else if (filter === 'monthly') {
+        startDate = new Date();
+        startDate.setMonth(now.getMonth() - 1);
+    } else if (filter === 'yearly') {
+        startDate = new Date();
+        startDate.setFullYear(now.getFullYear() - 1);
+    } else {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Invalid filter type');
+    }
+
+    const users = await User.find({ createdAt: { $gte: startDate } });
+    if (users.length === 0) {
+        throw new ApiError(StatusCodes.NOT_FOUND, 'No new user found');
+    }
+    return users;
 };
 
 export const AdminService = {
